940da630fcb2275a54f7f6031962ccd00575d5fe
helenen <helene.niassy@gmail.com>
2019-02-20 23:17:11 +0100

    error

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,22 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 const Style = {
-    a:{ 
-        textDecoration: 'none'
-    } 
-}
+  a: {
+    textDecoration: "none"
+  }
+};
 const Header = () => (
-    <div>
-      <Link href="./index">
-        <a style={Style}>Home</a>
-      </Link>
-      <Link href="/about">
-        <a style={Style}>About</a>
-      </Link>
-      <Link  href="./work">
-        <a style={Style}>works</a>
-      </Link>
-    </div>
-  )
-  
-  export default Header
+  <div className="nabvar">
+    <Link href="./index">
+      <a style={Style}>Home</a>
+    </Link>
+    <Link href="/about">
+      <a style={Style}>About</a>
+    </Link>
+    <Link href="./work">
+      <a style={Style}>works</a>
+    </Link>
+  </div>
+);
+
+export default Header;
--- header.test.js
+++ header.test.js
@@ -3,15 +3,19 @@ const { JSDOM } = jsdom;
 
 describe("I would see the homepage", () => {
   it("I would have a navbar", () => {
-    const frag = JSDOM.fragment(`
-    <a style={Style}>Home</a>
-    <a style={Style}>About</a>
-    <a style={Style}>works</a>
+    const nav = new JSDOM(`
+    <navbar>
+      <a style={Style}>Home</a>
+      <a style={Style}>About</a>
+      <a style={Style}>works</a>
+    </navbar>
    `);
-    expect(frag).toEqual(
-      `<a style={Style}>Home</a>
-       <a style={Style}>About</a>
-       <a style={Style}>works</a>`
-    );
+    expect(nav).toEqual(`
+    <navbar>
+      <a style={Style}>Home</a>
+      <a style={Style}>About</a>
+      <a style={Style}>works</a>
+     </navbar>
+    `);
   });
 });
